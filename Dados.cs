8a46fa9aa7bb634109ecc77ee52a5f432444df5f
Vinicius Pinheiro <carraravinicius155@gmail.com>
2022-06-14 14:04:18 -0400

    Primeira entrada de dados

---
+++ Dados.cs
@@ -0,0 +1,52 @@
+﻿
+using System;
+namespace Exercicos_de_Pogramação__Seção_5_
+{
+    public class Dados
+    {
+        private string _nome;
+        public double Conta { get; private set; }
+        private double Saldo;
+
+     
+
+        public Dados(string nome, double conta)
+        {
+            _nome = nome;
+            Conta = conta;
+            Saldo = 0;
+
+        }
+        public string GetNome()
+        {
+            return _nome;
+        }
+      public void SetNome(string nome)
+        {
+            if (_nome != null && _nome.Length > 1)
+            {
+                _nome = nome;
+            }
+        }
+
+        public void Saque(double saque)
+        {
+            Saldo = Saldo - (saque + 5);
+        }
+
+        public void Dep(double deposito)
+        {
+            Saldo += deposito;
+        }
+
+        public override string ToString()
+        {
+            return
+                "Usuario = " + _nome + "\n" +
+                "N° da Conta = " + Conta + "\n" +
+                "Saldo = " + Saldo + "\n";
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Exercicos_de_Pogramação__Seção_5_;
+{
+
+
+    Console.WriteLine("Digite o nome do usuario ");
+    var nome = Console.ReadLine();
+
+    Console.WriteLine("Digite o numero de sua conta");
+    var conta = double.Parse(Console.ReadLine());
+
+    Dados p = new Dados(nome, conta);
+    Console.WriteLine(p);
+
+    var r = "Deseja realizar um saque, um depósito ou sair do programa? \n" +
+        "Caso deseje saque digite 'saque' \n" +
+        "Caso deseje depósito digite 'dep' \n" +
+        "Caso deseje sair digite sair! \n ";
+
+    Console.WriteLine(r);
+    var resposta = Console.ReadLine();
+
+    if (resposta == "saque")
+    {
+        Console.WriteLine("Digite o valor a ser sacado da conta! \n");
+        var saque = double.Parse(Console.ReadLine());
+        p.Saque(saque);
+
+        Console.WriteLine(p);
+
+    }
+
+    else if (resposta == "dep")
+    {
+        Console.WriteLine("Digite o valor a ser depositado") ;
+        var dep = double.Parse(Console.ReadLine());
+        p.Dep(dep);
+        Console.WriteLine(p);
+    }
+    else
+    {
+        Console.WriteLine("Tenha um bom dia meu consagrado!!");
+    }
+
+
+
+}
+
+
